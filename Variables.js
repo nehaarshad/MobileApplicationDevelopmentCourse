eb0351dd442218c908a53fd8cb211eb7c40bdc7d
NehaArshad <nehaarshad1203@gmail.com>
2025-02-20 09:52:08 +0500

    Hoisting in JS

--- Variables.js
+++ Variables.js
@@ -1,16 +1,11 @@
-const name="Neha" //global variable //const cant change
-var age=25 //global variable
-//name="Ayesha" //Assignment to constant variable. //const cant change
-//final variable
-
-function greet(name,age){  //function
-    if(age>20){  //blockScopeVariable
-        let info="Study in BSIT" //local variable
-        console.log(`Hello ${name} ${info} ${age}`)
-    }
-  //  console.log(`Hello ${name} ${info} ${age}`) //info is not defined
+console.log(name); // undefined
+//console.log(age); //ReferenceError: Cannot access 'age' before initialization
+console.log(getName); // undefined
+var name = 'Neha';
+let age = 30;
+console.log(age); // 30
+console.log(name); // Neha
+getName();
+function getName() {
+    console.log('Neha');
 }
-
-console.log(typeof(name)) //Neha
-console.log(typeof(age)) //
-greet("Ayesha",22) //Hello Ayesha Study in BSSE 22
