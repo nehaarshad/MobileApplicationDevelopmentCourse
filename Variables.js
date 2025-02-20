17737c60a4e072f559198a1532094548ae74c4f9
NehaArshad <nehaarshad1203@gmail.com>
2025-02-20 09:43:26 +0500

    BlockScope Variables and tupeOfMethod Use

--- Variables.js
+++ Variables.js
@@ -1,12 +1,16 @@
 const name="Neha" //global variable //const cant change
-let age=25 //global variable
+var age=25 //global variable
+//name="Ayesha" //Assignment to constant variable. //const cant change
+//final variable
 
 function greet(name,age){  //function
-    var info="Study in BSSE" //local variable
-    console.log(`Hello ${name} ${info} ${age}`)
+    if(age>20){  //blockScopeVariable
+        let info="Study in BSIT" //local variable
+        console.log(`Hello ${name} ${info} ${age}`)
+    }
+  //  console.log(`Hello ${name} ${info} ${age}`) //info is not defined
 }
 
-console.log(name) //Neha
-console.log(age) //25
+console.log(typeof(name)) //Neha
+console.log(typeof(age)) //
 greet("Ayesha",22) //Hello Ayesha Study in BSSE 22
-console.log(info) //info is not defined
